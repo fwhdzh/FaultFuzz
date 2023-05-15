b80e869f5e054e9e2aaca3d3d72b8289d4ab8348
fwhdzh <1064039735@qq,com>
2023-05-15 19:03:10 +0800

    modify script for server-60

--- script/controller-bash/zk-3.8.1-60/copy-target-and-starter-to-alone-folder.sh
+++ script/controller-bash/zk-3.8.1-60/copy-target-and-starter-to-alone-folder.sh
@@ -1,6 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-DEST_DIR=/data/fengwenhan/data/crashfuzz_zk_jar_and_start
+DEST_DIR=/home/fengwenhan/data/crashfuzz_zk_jar_and_start
 
 cp /home/fengwenhan/code/crashfuzz-ctrl/crashfuzz/target/CCrashFuzzer-0.0.1-SNAPSHOT.jar $DEST_DIR
 cp $SCRIPT_DIR/crashfuzzer-alone.sh $DEST_DIR
