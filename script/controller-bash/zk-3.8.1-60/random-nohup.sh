0b280eb01d5e706e0a4ca253d9a45bde8a56b718
fwhdzh <fengwenhan21@otcaix.iscas.ac.cn>
2023-05-17 23:20:38 +0800

    add script for hbase-2.4.11-60

--- script/controller-bash/zk-3.8.1-60/random-nohup.sh
+++ script/controller-bash/zk-3.8.1-60/random-nohup.sh
@@ -1,3 +1,3 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-nohup $SCRIPT_DIR/random-alone.sh > /data/fengwenhan/data/crashfuzz_zk_logs/crashfuzz_zk_nohup.log &
+nohup $SCRIPT_DIR/random-alone.sh > /home/fengwenhan/data/crashfuzz_zk_logs/crashfuzz_zk_nohup.log &
